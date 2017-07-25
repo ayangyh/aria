eaeda5547b50a568ec6a044381792e97669b0c3d
Angela Yang <angelayangyh@gmail.com>
2017-07-25 11:34:50 -0700

    checkpoint 23 dom collection view: completed

---
+++ scripts/collection.js
@@ -0,0 +1,24 @@
+var collectionItemTemplate =
+'<div class="collection-album-container column fourth">'
++ '    <img src="assets/images/album_covers/01.jpg"/>'
++ '    <div class="collection-album-info caption">'
++ '        <p>'
++ '            <a class="album-name" href="album.html">I See You</a>'
++ '            <br/>'
++ '            <a class="album-artist" href="album.html">The xx</a>'
++ '            <br/>'
++ '        </p>'
++ '    </div>'
++ '</div>'
+;
+
+window.onload = function() {
+  
+  var collectionContainer = document.getElementsByClassName('album-covers')[0];
+
+  collectionContainer.innerHTML = '';
+
+  for (var i = 0; i < 12; i++) {
+    collectionContainer.innerHTML += collectionItemTemplate;
+  }
+}
